a08724997cb4f67d14bd0a982e9eab0fbb93258e
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-01-15 14:09:58 +0900

    各種アセット追加

---
+++ Assets/Script/BombController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    public float bombSpeed;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.bombSpeed,0,0);
+        if(transform.position.x>=10.0f)Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Block")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
