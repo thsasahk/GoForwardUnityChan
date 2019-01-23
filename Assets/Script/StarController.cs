c2fad13eee088413c7dc35507122f4a8b00a42d0
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-01-24 00:47:38 +0900

    一応記録
    挙動がいまいち綺麗にならないので変更予定

---
+++ Assets/Script/StarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    public float fallTime;
+    public float stopPosY;
+    public float stopPosX;
+    public float rollAngle;
+    public float speedX;
+    public float speedY;
+    void Start()
+    {
+        iTween.MoveTo(gameObject,iTween.Hash("x",this.stopPosX,"y",this.stopPosY,"time",this.fallTime));
+        iTween.MoveAdd(this.gameObject,iTween.Hash("x",this.speedX*Mathf.Cos(this.transform.eulerAngles.z * Mathf.Deg2Rad),
+        "y",this.speedY*Mathf.Sin(this.transform.eulerAngles.z * Mathf.Deg2Rad),"time",this.fallTime));
+        iTween.RotateBy(gameObject,iTween.Hash("z",this.rollAngle,"time",this.fallTime));
+        
+    }
+
+    
+    void Update()
+    {
+    }
+}
+
