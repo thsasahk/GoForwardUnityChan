40d80efbfa161722cd500e6d11e52b0bc414bfab
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-02-03 00:37:32 +0900

    chargesliderをPrefab化、TutorialSceneでCube,CubeGenaratorを管理するScriptを作成

---
+++ Assets/Script/TSCubeController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TSCubeController : MonoBehaviour
+{
+    /// <summary>
+    /// TutorialSceneManagerオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject tutorialSceneManager;
+    /// <summary>
+    /// TutorialSceneManagerオブジェクトのスクリプト
+    /// </summary>
+    private TutorialSceneManagerController tutorialSceneManagerController;
+    /// <summary>
+    /// CubeGeneratorオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject cubeGenerator;
+    /// <summary>
+    /// TutorialSceneManagerオブジェクトのスクリプト
+    /// </summary>
+    private TSCubeGeneratorController tSCubeGeneratorController;
+    /// <summary>
+    /// オブジェクトの破棄条件を管理する変数
+    /// </summary>
+    [SerializeField] private int life = 3;
+    /// <summary>
+    /// キューブの移動速度
+    /// </summary>
+    public float speed = -0.2f;
+    /// <summary>
+    /// 消滅位置
+    /// </summary>
+    public float deadLine = -10;
+    /// <summary>
+    /// オブジェクトのAudioSource
+    /// </summary>
+    private AudioSource[] SE;
+    /// <summary>
+    /// オブジェクト破棄時に生成されるParticleSystem
+    /// </summary>
+    public GameObject particlePrefab;
+
+
+    void Start()
+    {
+        this.tutorialSceneManager = GameObject.Find("TutorialSceneManager");
+        this.cubeGenerator = GameObject.Find("CubeGenerator");
+        this.tutorialSceneManagerController = this.tutorialSceneManager.GetComponent<TutorialSceneManagerController>();
+        this.tSCubeGeneratorController = this.cubeGenerator.GetComponent<TSCubeGeneratorController>();
+    }
+
+    void Update()
+    {
+        switch (this.tutorialSceneManagerController.lesson)
+        {
+            case 2:
+                this.life = 3;
+                break;
+
+            case 4:
+                //キューブを移動させる
+                transform.Translate(this.speed, 0, 0);
+                //画面外に出たら破棄する
+                if (transform.position.x < deadLine)
+                {
+                    Destroy(this.gameObject);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// オブジェクトのlife変数を変化させオブジェクトの破棄を管理する
+    /// </summary>
+    /// <param name="i">bombPrefabの攻撃力</param>
+    public void Damage(int i)
+    {
+        //HardBlockオブジェクトの場合はlife変数は一定値
+        if (gameObject.tag == "HBlock")
+        {
+            this.SE[1].Play();
+            return;
+        }
+        //life変数を引数分マイナスさせる
+        this.life -= i;
+        //life変数が1未満の場合はオブジェクトを破棄する
+        if (this.life < 1)
+        {
+            Instantiate(this.particlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        this.tutorialSceneManagerController.lesson++;
+        this.tSCubeGeneratorController.createCube = false;
+    }
+}
---
+++ Assets/Script/TSCubeGeneratorController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TSCubeGeneratorController : MonoBehaviour
+{
+    /// <summary>
+    /// TutorialSceneManagerオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject tutorialSceneManager;
+    /// <summary>
+    /// TutorialSceneManagerオブジェクトのスクリプト
+    /// </summary>
+    private TutorialSceneManagerController tutorialSceneManagerController;
+    /// <summary>
+    /// 生成するオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject[] cube;
+    /// <summary>
+    /// Cubeの生成を管理する変数
+    /// </summary>
+    public bool createCube = false;
+
+    void Start()
+    {
+        this.tutorialSceneManagerController = this.tutorialSceneManager.GetComponent<TutorialSceneManagerController>();
+    }
+
+    void Update()
+    {
+        if (this.createCube == false)
+        {
+            CreateCube();
+        }
+    }
+
+    /// <summary>
+    /// lesson変数を参照しながら決められたキューブを生成する
+    /// </summary>
+    void CreateCube()
+    {
+        this.createCube = true;
+        switch (this.tutorialSceneManagerController.lesson)
+        {
+            case 1:
+            case 2:
+                Instantiate(this.cube[0], new Vector2(5.0f, 0.0f), Quaternion.identity);
+                break;
+
+            case 3:
+                Instantiate(this.cube[1], new Vector2(5.0f, 0.0f), Quaternion.identity);
+                Instantiate(this.cube[0], new Vector2(7.0f, 0.0f), Quaternion.identity);
+                break;
+
+            case 4:
+                Instantiate(this.cube[0], new Vector2(10.0f, 0.0f), Quaternion.identity);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
