b4c4bd137de879712f03e9cdfd5d7729929f79c2
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-02-06 22:33:45 +0900

    Scriptにコメントを追加

--- Assets/Script/GameOverTextColorController.cs
+++ Assets/Script/GameOverTextColorController.cs
@@ -9,10 +9,21 @@ public class GameOverTextColorController : MonoBehaviour
     /// オブジェクトの色の変化を管理する
     /// </summary>
     public Gradient gradiate;
+    /// <summary>
+    /// オブジェクトのTextMeshProUGUI
+    /// </summary>
     private TextMeshProUGUI tmp;
-    private Color color;
+    /// <summary>
+    /// オブジェクトのMaterial
+    /// </summary>
     private Material material;
+    /// <summary>
+    /// Canvasオブジェクト
+    /// </summary>
     [SerializeField] private GameObject canvas;
+    /// <summary>
+    /// Canvasオブジェクトのスクリプト
+    /// </summary>
     private UIController uIController;
 
     void Start()
@@ -24,16 +35,21 @@ public class GameOverTextColorController : MonoBehaviour
 
     void Update()
     {
+        //Clear時に表示されるテキストのUnderlayColorを変更する
         if (this.uIController.clear)
         {
             this.material.SetColor(ShaderUtilities.ID_UnderlayColor, ColorGradiate());
         }
     }
 
+    /// <summary>
+    /// 0～1の値をとる変数iをgradiate.Evaluateの引数としてColor型の変数を返す
+    /// </summary>
+    /// <returns></returns>
     private Color ColorGradiate()
     {
         float i = Mathf.Abs(Mathf.Sin(Time.time));
-        this.color = this.gradiate.Evaluate(i);
-        return this.color;
+        Color color = this.gradiate.Evaluate(i);
+        return color;
     }
 }
