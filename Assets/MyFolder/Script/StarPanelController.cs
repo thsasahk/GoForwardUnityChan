7f74e5ab3a0c3bab8c0f4a01bfe1a490bb707ab5
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-03-10 01:28:30 +0900

    starBullet変数の値によって子のimageオブジェクトを点灯させる

--- Assets/MyFolder/Script/StarPanelController.cs
+++ Assets/MyFolder/Script/StarPanelController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StarPanelController : MonoBehaviour
 {
@@ -12,19 +13,63 @@ public class StarPanelController : MonoBehaviour
     /// Playerオブジェクトのスクリプト
     /// </summary>
     private UnityChanController unityChanController;
+    /// <summary>
+    /// 子オブジェクト
+    /// </summary>
+    [SerializeField] private Image[] image;
+    /// <summary>
+    /// StarShot()を行える回数
+    /// </summary>
+    public int starBullet = 0;
+
+    //private int starBullet2 = 0;
+    /// <summary>
+    /// StarBulletを発射可能であることを示す色
+    /// </summary>
+    private Color color;
+    /// <summary>
+    /// StarBullet発射不可を示す色
+    /// </summary>
+    private Color color2;
 
     void Start()
     {
         this.unityChanController = this.player.GetComponent<UnityChanController>();
+        this.color = new Color(255, 255, 255, 255);
+        this.color2 = new Color(0, 0, 0, 255);
     }
 
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        //StarBulletの値によって何個目のイメージまで点灯させるかを決定する
+        for (int i = 1; i <= this.starBullet; i++)
+        {
+            this.image[i - 1].color = this.color;
+        }
+        for (int m = this.image.Length; m > this.starBullet; m--)
+        {
+            this.image[m - 1].color = this.color2;
+        }
+        /*
+        if (this.starBullet != this.starBullet2)
+        {
+            for(int i = 1; i <= this.starBullet; i++)
+            {
+                this.image[i - 1].color = this.color;
+            }
+            for(int m = this.image.Length; m > this.starBullet; m--)
+            {
+                this.image[m - 1].color = this.color2;
+            }
+        }
+        */
+        //ZキーでStarBulletを発射する
+        if (Input.GetKeyDown(KeyCode.Z))
         {
             this.unityChanController.StarShot();
             this.unityChanController.starShot = false;
         }
+        //this.starBullet2 = this.starBullet;
     }
 }
