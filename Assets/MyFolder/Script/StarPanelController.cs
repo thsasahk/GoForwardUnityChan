6805693ef536f3a146584867bb04b7edeb2a42c4
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-03-09 11:56:24 +0900

    StarPanelControllerを制作

---
+++ Assets/MyFolder/Script/StarPanelController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarPanelController : MonoBehaviour
+{
+    /// <summary>
+    /// Playerオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject player;
+    /// <summary>
+    /// Playerオブジェクトのスクリプト
+    /// </summary>
+    private UnityChanController unityChanController;
+
+    void Start()
+    {
+        this.unityChanController = this.player.GetComponent<UnityChanController>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.unityChanController.StarShot();
+            this.unityChanController.starShot = false;
+        }
+    }
+}
