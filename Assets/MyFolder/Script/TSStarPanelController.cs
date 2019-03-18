e6d1b2208edece326e6ad410a817fd61adc485f7
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-03-19 02:29:00 +0900

    TutorialSceneにStarPanelオブジェクトを追加

---
+++ Assets/MyFolder/Script/TSStarPanelController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TSStarPanelController : MonoBehaviour
+{
+    /// <summary>
+    /// Playerオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject player;
+    /// <summary>
+    /// Playerオブジェクトのスクリプト
+    /// </summary>
+    private TSPlayerController tSPlayerController;
+    /// <summary>
+    /// 子オブジェクト
+    /// </summary>
+    [SerializeField] private Image[] image;
+    /// <summary>
+    /// StarDash()を行える回数
+    /// </summary>
+    public int starCount = 0;
+    //private int starBullet2 = 0;
+    /// <summary>
+    /// StarBulletを発射可能であることを示す色
+    /// </summary>
+    private Color color;
+    /// <summary>
+    /// StarBullet発射不可を示す色
+    /// </summary>
+    private Color color2;
+
+    void Start()
+    {
+        this.tSPlayerController = this.player.GetComponent<TSPlayerController>();
+        this.color = new Color(255, 255, 255, 255);
+        this.color2 = new Color(0, 0, 0, 255);
+    }
+
+
+    void Update()
+    {
+        //StarBulletの値によって何個目のイメージまで点灯させるかを決定する
+        for (int i = 1; i <= this.starCount; i++)
+        {
+            this.image[i - 1].color = this.color;
+        }
+        for (int m = this.image.Length; m > this.starCount; m--)
+        {
+            this.image[m - 1].color = this.color2;
+        }
+        /*
+        if (this.starBullet != this.starBullet2)
+        {
+            for(int i = 1; i <= this.starBullet; i++)
+            {
+                this.image[i - 1].color = this.color;
+            }
+            for(int m = this.image.Length; m > this.starBullet; m--)
+            {
+                this.image[m - 1].color = this.color2;
+            }
+        }
+        */
+        //Zキーで前進する
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            this.tSPlayerController.StarDash();
+            //this.tSPlayerController.starDash = false;
+        }
+    }
+}
---
+++ Assets/MyFolder/Script/TSStarpanelTextController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TSStarpanelTextController : MonoBehaviour
+{
+    /// <summary>
+    /// StarPanelオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject starPanel;
+    /// <summary>
+    /// StarPanelオブジェクトのスクリプト
+    /// </summary>
+    private TSStarPanelController tSStarPanelController;
+    /// <summary>
+    /// オブジェクトのフォントマテリアルの器
+    /// </summary>
+    private Material material;
+    /// <summary>
+    /// オブジェクトのTextMeshProUGUI
+    /// </summary>
+    private TextMeshProUGUI tmp;
+    /// <summary>
+    /// オブジェクトのGradient
+    /// </summary>
+    public Gradient gradiate;
+    /// <summary>
+    /// ColorGradiate()関数の返し値に使用
+    /// </summary>
+    private Color color;
+    /// <summary>
+    /// Gradien.Evauluent()の引数
+    /// </summary>
+    private float i;
+
+    void Start()
+    {
+        this.tSStarPanelController = this.starPanel.GetComponent<TSStarPanelController>();
+        this.tmp = GetComponent<TextMeshProUGUI>();
+        this.material = this.tmp.fontMaterial;
+    }
+
+    void Update()
+    {
+        if (this.tSStarPanelController.starCount != 0)
+        {
+            this.tmp.text = "クリックorZキー";
+            this.material.SetColor(ShaderUtilities.ID_UnderlayColor, ColorGradiate());
+        }
+        else
+        {
+            this.tmp.text = "";
+        }
+    }
+
+    private Color ColorGradiate()
+    {
+        this.i = Mathf.Abs(Mathf.Sin(Time.time));
+        this.color = this.gradiate.Evaluate(this.i);
+        return this.color;
+    }
+}
