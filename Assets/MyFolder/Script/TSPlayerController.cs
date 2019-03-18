0b4f4d2f02b4292eed20c92e8c982c7efc0b2a0b
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-03-19 02:40:43 +0900

    StarDash()を追加

--- Assets/MyFolder/Script/TSPlayerController.cs
+++ Assets/MyFolder/Script/TSPlayerController.cs
@@ -92,7 +92,7 @@ public class TSPlayerController : MonoBehaviour
     /// <summary>
     /// オブジェクトの接地状態を入力する
     /// </summary>
-    private bool isGround;
+    [SerializeField] private bool isGround;
     /// <summary>
     /// ジェットのSEが再生中かどうかを判別
     /// </summary>
@@ -104,11 +104,107 @@ public class TSPlayerController : MonoBehaviour
     /// <summary>
     /// ポーズ中であることを示す変数
     /// </summary>
-    private bool isPause = false;
+    [SerializeField] private bool isPause = false;
     /// <summary>
     /// ジャンプゲージの回復速度に影響
     /// </summary>
     [SerializeField] private float recovery;
+    /// <summary>
+    /// StarDash()の間隔
+    /// </summary>
+    public float coolTime;
+    /// <summary>
+    /// StarDash()を最後に呼び出してからの時間を計測
+    /// </summary>
+    [SerializeField] private float time = 0;
+    /// <summary>
+    /// StarPanelオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject TSStarPanel;
+    /// <summary>
+    /// StarPanelControllerオブジェクト
+    /// </summary>
+    private TSStarPanelController tSStarPanelController;
+    /// <summary>
+    /// Playerオブジェクトのx方向の限界
+    /// </summary>
+    public float maxPosX;
+    /// <summary>
+    /// dashParticleオブジェクト
+    /// </summary>
+    [SerializeField] private ParticleSystem dashParticle;
+    /// <summary>
+    /// StarDash()による短時間の無敵
+    /// </summary>
+    public bool isStar = false;
+    /// <summary>
+    /// 無敵の継続時間
+    /// </summary>
+    [SerializeField] private float starTime;
+    /// <summary>
+    /// StarDash()内のiTween.MoveToの継続時間に影響
+    /// </summary>
+    [SerializeField] private float t1;
+    /// <summary>
+    /// dashParticleオブジェクトのAudioSource
+    /// </summary>
+    private AudioSource dashSE;
+    /// <summary>
+    /// StarShot()を呼び出すためのクリックした際にJump()を呼ぶのを禁止する
+    /// </summary>
+    //public bool starDash;
+    /// <summary>
+    /// 連射機対策のショットする際に必要な間隔
+    /// </summary>
+    [SerializeField] private float interval;
+    /// <summary>
+    /// 前回ショットしてからの時間を計測
+    /// </summary>
+    private float shotTime = 0;
+    /// <summary>
+    /// 極めて規則的な入力をされていないかを確認
+    /// </summary>
+    private float shotTime2;
+    /// <summary>
+    /// 接触したオブジェクトに与えるy方向の力
+    /// </summary>
+    private float verticalPower;
+    /// <summary>
+    /// verticalPowerの最小値
+    /// </summary>
+    [SerializeField] private float vPowerMin;
+    /// <summary>
+    /// verticalPowerの最大値
+    /// </summary>
+    [SerializeField] private float vPowerMax;
+    /// <summary>
+    /// 接触したオブジェクトに与えるx方向の力
+    /// </summary>
+    private float horizontalPower;
+    /// <summary>
+    /// horizontalPowerの最小値
+    /// </summary>
+    [SerializeField] private float hPowerMin;
+    /// <summary>
+    /// horizontalPowerの最大値
+    /// </summary>
+    [SerializeField] private float hPowerMax;
+    /// <summary>
+    /// 接触したオブジェクトに与える回転速度
+    /// </summary>
+    private float rollSpeed;
+    /// <summary>
+    /// rollSpeedの最小値
+    /// </summary>
+    [SerializeField] private float rsMin;
+    /// <summary>
+    /// rollSpeedの最大値
+    /// </summary>
+    [SerializeField] private float rsMax;
+    /// <summary>
+    /// 接触したオブジェクトが落下を続ける時間
+    /// </summary>
+    [SerializeField] private float fallTime;
 
     void Start()
     {
@@ -120,6 +216,9 @@ public class TSPlayerController : MonoBehaviour
         this.animator = GetComponent<Animator>();
         this.rigid2D = GetComponent<Rigidbody2D>();
         this.jetParticle = this.jet.GetComponent<ParticleSystem>();
+        //this.TSStarPanel = GameObject.FindGameObjectWithTag("StarPanel");
+        this.tSStarPanelController = this.TSStarPanel.GetComponent<TSStarPanelController>();
+        this.dashSE = this.dashParticle.GetComponent<AudioSource>();
     }
 
     void Update()
@@ -138,6 +237,16 @@ public class TSPlayerController : MonoBehaviour
         {
             this.unitySE[1].Play();
             this.isPause = false;
+        }
+        //最後にStarDash()を呼び出してからの時間を計測
+        this.time += Time.deltaTime;
+        //最後に射撃してからの時間を計測
+        this.shotTime += Time.deltaTime;
+        //無敵状態解除
+        if (this.time >= this.starTime)
+        {
+            this.dashParticle.Stop();
+            this.isStar = false;
         }
         //定位置に戻ったら操作を可能にする
         if (transform.position.x >= -2.9f)
@@ -160,6 +269,15 @@ public class TSPlayerController : MonoBehaviour
             this.jetParticle.Play();
             this.rigid2D.velocity = Vector2.zero;
             return;
+        }
+        //最後にStarDash()を呼び出してからの時間を計測
+        this.time += Time.deltaTime;
+        this.shotTime += Time.deltaTime;
+        //無敵状態解除
+        if (this.time >= this.starTime)
+        {
+            this.dashParticle.Stop();
+            this.isStar = false;
         }
         /*
         //チャージ音を再生する
@@ -217,7 +335,7 @@ public class TSPlayerController : MonoBehaviour
         //
         if (!Input.GetMouseButton(0) && !Input.GetKey(KeyCode.Space))
         {
-            this.jumpTime -= Time.deltaTime * 1.5f;
+            this.jumpTime -= Time.deltaTime * this.recovery;
             if (this.jumpTime < 0.0f)
             {
                 this.jumpTime = 0.0f;
@@ -364,7 +482,7 @@ public class TSPlayerController : MonoBehaviour
     private void LateUpdate()
     {
         if (this.tutorialSceneManagerController.loadScene || this.isComeBack ||
-            Mathf.Approximately(Time.timeScale, 0f))
+            Mathf.Approximately(Time.timeScale, 0f)|| this.isStar)
         {
             return;
         }
@@ -404,28 +522,33 @@ public class TSPlayerController : MonoBehaviour
     /// </summary>
     void Shot()
     {
-        this.unitySE[0].Stop();
-        switch (this.chargeLV)
+        if (this.shotTime >= interval && this.shotTime != this.shotTime2)
         {
-            case 0:
-                Instantiate(bomb[0], new Vector2(transform.position.x + 1.0f, transform.position.y - 0.3f),
-                    Quaternion.identity);
-                break;
+            this.unitySE[0].Stop();
+            switch (this.chargeLV)
+            {
+                case 0:
+                    Instantiate(bomb[0], new Vector2(transform.position.x + 1.0f, transform.position.y - 0.3f),
+                        Quaternion.identity);
+                    break;
 
-            case 1:
-                GameObject BombLv1 = Instantiate(this.bomb[1]) as GameObject;
-                BombLv1.transform.position = new Vector2(transform.position.x + 1.0f, transform.position.y - 0.3f);
-                BombLv1.transform.Rotate(0.0f, 0.0f, 0.0f);
-                break;
+                case 1:
+                    GameObject BombLv1 = Instantiate(this.bomb[1]) as GameObject;
+                    BombLv1.transform.position = new Vector2(transform.position.x + 1.0f, transform.position.y - 0.3f);
+                    BombLv1.transform.Rotate(0.0f, 0.0f, 0.0f);
+                    break;
 
-            case 2:
-                GameObject BombLv2 = Instantiate(this.bomb[2]) as GameObject;
-                BombLv2.transform.position = new Vector2(transform.position.x + 1.0f, transform.position.y - 0.3f);
-                BombLv2.transform.Rotate(0.0f, 0.0f, 0.0f);
-                break;
+                case 2:
+                    GameObject BombLv2 = Instantiate(this.bomb[2]) as GameObject;
+                    BombLv2.transform.position = new Vector2(transform.position.x + 1.0f, transform.position.y - 0.3f);
+                    BombLv2.transform.Rotate(0.0f, 0.0f, 0.0f);
+                    break;
+            }
+            this.chargeTime = 0.0f;
+            this.chargeLV = 0;
+            this.shotTime2 = this.shotTime;
+            this.shotTime = 0;
         }
-        this.chargeTime = 0.0f;
-        this.chargeLV = 0;
     }
 
     /// <summary>
@@ -566,5 +689,109 @@ public class TSPlayerController : MonoBehaviour
         this.animator.SetBool("Run", false);
         iTween.MoveTo(gameObject, iTween.Hash("y", 1.0f));
         iTween.MoveTo(gameObject, iTween.Hash("x", -2.9f, "y", 1.0f, "time", 3.0f, "delay", 1.0f, "easeType", "linear"));
+    }
+
+    /// <summary>
+    /// x正方向への力を与える
+    /// </summary>
+    public void StarDash()
+    {
+        /*
+        Debug.Log("ok2");
+        if (this.time < this.coolTime)
+        {
+            Debug.Log(1);
+            return;
+        }
+        if (this.tSStarPanelController.starCount < 1)
+        {
+            Debug.Log(2);
+            return;
+        }
+        if (this.isPause)
+        {
+            Debug.Log(3);
+            return;
+        }
+        if (!isGround)
+        {
+            Debug.Log(4);
+            return;
+        }
+        if (this.isComeBack)
+        {
+            Debug.Log(5);
+            return;
+        }
+        */
+        if (this.time < this.coolTime || this.tSStarPanelController.starCount < 1 
+            || this.isPause || !isGround|| this.isComeBack)
+        {
+            Debug.Log("no");
+            return;
+        }
+        this.dashParticle.Play();
+        this.dashSE.Play();
+        this.isStar = true;
+        this.tSStarPanelController.starCount--;
+        //this.starDash = true;
+        //this.isStar = true;
+        //this.rigid2D.AddForce(this.dashPower);
+        iTween.MoveTo(gameObject, iTween.Hash("x", this.maxPosX, "time", this.starTime - this.t1,
+            /*"delay", 0.2f,*/ "easeType", "linear"));
+        this.time = 0;
+        this.chargeTime = 0.0f;
+        this.chargeLV = 0;
+        /*
+        //残談がない時やクリアシーンに入った時は発射不可
+        if (this.starPanelController.starBullet < 1|| this.uiController.clearScene)
+        {
+            return;
+        }
+        //残談を減らす
+        this.starPanelController.starBullet--;
+        this.starShot = true;
+        this.starBullePosition = new Vector2(this.transform.position.x + this.x1, this.transform.position.y + this.y1);
+        Instantiate(this.starBullet, this.starBullePosition, Quaternion.identity);
+        */
+    }
+
+    /*
+    /// <summary>
+    /// Jump()を呼び出すことを許可する
+    /// </summary>
+    public void StarDashEnd()
+    {
+        //this.starDash = false;
+        this.isStar = false;
+    }
+    */
+
+    /// <summary>
+    /// 接触したオブジェクトに力を与えて吹き飛ばす
+    /// </summary>
+    /// <param name="other">接触したオブジェクト</param>
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (this.isStar)
+        {
+            switch (other.gameObject.tag)
+            {
+                case "Block":
+                case "HBlock":
+                    this.verticalPower = Random.Range(this.vPowerMin, this.vPowerMax);
+                    this.horizontalPower = Random.Range(this.hPowerMin, this.hPowerMax);
+                    other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(this.horizontalPower * Time.deltaTime,
+                        this.verticalPower * Time.deltaTime));
+                    this.rollSpeed = Random.Range(this.rsMin, this.rsMax);
+                    iTween.RotateTo(other.gameObject, iTween.Hash("z", this.rollSpeed, "time", this.fallTime));
+                    other.gameObject.GetComponent<TSCubeController>().speed = 0;
+                    other.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
